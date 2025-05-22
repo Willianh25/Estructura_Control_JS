22b0daf752d1922479ed80434ee21049eb370fc6
Willian Herrera <wherrera4@gmail.com>
2025-05-21 22:54:28 -0500

    Se agrega lineas divisoras, en el js tablasSeleccionadas

--- public/js/tablasSeleccionadas.js
+++ public/js/tablasSeleccionadas.js
@@ -4,17 +4,18 @@ let tipo = prompt("¿Qué tablas quieres ver? Escribe 'pares' o 'impares':");
 if (tipo === "pares" || tipo === "impares") {
     for (let numero = 1; numero <= 10; numero++) {
         if (tipo === "pares" && numero % 2 === 0) {
+            console.log("---------------");
             console.log("Tabla del " + numero);
             for (let i = 1; i<= 10; i++) {
                 console.log(numero + " x " + i + " = " + (numero * i));
             }
             console.log("---------------");
         }else if (tipo === "impares" && numero % 2 !== 0){
+            console.log("---------------");
             console.log("Table del " + numero);
             for (let i = 1; i <= 10; i++) {
                 console.log(numero + " x " + i + " = " + (numero * i));
             }
-            console.log("---------------");
         }
     }
 } else {
